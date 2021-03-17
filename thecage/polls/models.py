b41595f9fdb3be2b5b6c419ef57a6f242d35fb2b
Evan Morris <evanmorris97@gmail.com>
2021-03-17 16:04:54 +0000

    create the models as a group

--- thecage/polls/models.py
+++ thecage/polls/models.py
@@ -1,3 +1,31 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Name(models.Model):
+    name_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.name_text
+
+class Location(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    location_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.location_text
+
+class Description(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    description_text = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.description_text
+
+class DiscoveryDate(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    discoveryDate_text = models.DateTimeField("date discovered")
+
+    def __str__(self):
+        return self.discoveryDate_text
