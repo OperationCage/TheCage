b2f0e0a35eacffcfd53978e702218f248fb1775c
Evan Morris <evanmorris97@gmail.com>
2021-03-18 16:32:48 +0000

    added home navlink to navbar

--- thecage/polls/urls.py
+++ thecage/polls/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("cryptids/", views.cryptids, name='cryptids'),
     path("cryptids/<int:cryptid_id>/", views.cryptid, name="cryptid"),
-
     path("location/", views.location, name='location'),
     path("all_cryptids_here/", views.all_cryptids_here, name='all_cryptids_here'),
     path("cryptid_disc_date/", views.cryptid_disc_date, name='cryptid_disc_date'),
-    path("", views.home, name="home"),
 ]
