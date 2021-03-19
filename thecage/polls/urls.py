f6408f0e5308ed3adfb77ddaf101e476ff160aa5
Jim <jadavey@hotmail.com>
2021-03-19 12:20:37 +0000

    Images working and removed extra poetry install
    Fingers crossed

--- thecage/polls/urls.py
+++ thecage/polls/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path("cryptids/", views.cryptids, name='cryptids'),
     path("cryptids/<int:cryptid_id>/", views.cryptid, name="cryptid"),
-
     path("location/", views.location, name='location'),
     path("all_cryptids_here/", views.all_cryptids_here, name='all_cryptids_here'),
     path("cryptid_disc_date/", views.cryptid_disc_date, name='cryptid_disc_date'),
